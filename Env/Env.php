1225d98022bf916d64b041e765b5626e54e3d36d
gleb-bread <glebhets123@gmail.com>
2023-12-01 20:49:38 +0300

    com

--- Env/Env.php
+++ Env/Env.php
@@ -7,17 +7,13 @@ class Env {
 
 	public static ?string $site_login;
 	public static ?string $site_domain;
-	public static ?string $site_name;
 	public static ?string $site_url;
-	public static ?string $site_desc;
 	public static ?string $site_tlgrm_bot;
-	public static ?string $title;
-	public static ?string $description;
 	public static ?string $keywords;
 	public static ?string $smeta_dir;
 	public static ?array $word;
 	public static ?string $metrika;
-	public static ?string $ver = 'v0020';
+	public static ?string $ver = 'api';
 	public static ?string $hash;
 
 	public static function setAll() {
@@ -26,12 +22,8 @@ class Env {
 
 			self::$site_login = $site_login;
 			self::$site_domain = $site_domain;
-			self::$site_name = $site_name;
 			self::$site_url = $site_url;
-			self::$site_desc = $site_desc;
 			self::$site_tlgrm_bot = $site_tlgrm_bot;
-			self::$title = $title;
-			self::$description = $description;
 			self::$keywords = $keywords;
 			self::$smeta_dir = $smeta_dir;
 			self::$word = $word;
