e10faed09ed47b1f92a7300d84e34d0c98b0bba3
gleb-bread <glebhets123@gmail.com>
2023-12-15 11:33:08 +0300

    add finances

--- www/api/cart/cart.php
+++ www/api/cart/cart.php
@@ -2,6 +2,7 @@
 
 namespace API\cart;
 
+use Error;
 use \Error_\Error_;
 
 class cart extends \API\AController {
@@ -18,46 +19,64 @@ class cart extends \API\AController {
 		self::unsuported();
 	}
 
-	protected static function get() {
-		$userId = self::$user->user_id;
-        if($userId instanceof Error_) self::internalServerError();
-
+    protected static function patch() {
         $data = self::getParams();
         $data = self::getParamsWithoutUserToken($data);
-        $data['cart_uid'] = $userId;
+        $idCart = self::$_SPLIT[2];
 
-        $result = \Model\Cart::getAll($data);
-        if($result instanceof Error_) self::badRequest();
+        $cartProduct = \Model\Cart::get($idCart);
+        if($cartProduct instanceof Error_) self::badRequest();
 
-        $paramsList = array();
-        foreach($result as $key => $value){
-            array_push($paramsList, [
-                'cart_product_id'   => $value->cart_product_id,
-                'cart_is_parsing'   => $value->cart_is_parsing,
-                'cart_count'        => $value->cart_count,
-            ]);
-        }
-
-        $products = array();
+        $cartProduct->update($data);
+        if($cartProduct instanceof Error_) self::internalServerError();
         
-        foreach($paramsList as $key => $value){
-
-            $idProduct = $value['cart_product_id'];
-            $isParser = $value['cart_is_parsing'];
-            $countProduct = $value['cart_count'];
-
-            if($isParser != 0){
-                $product = \Model\ParsingProduct::get($idProduct);
-            }
-
-            $product->count_cart = $countProduct;
-
-            array_push($products, $product);
-        }
-
-        echo json_encode($products);exit;
+        echo json_encode($cartProduct);exit;
 
 	}
+
+	protected static function get() {
+        switch(self::$_SPLIT[2]){
+            case 'count': {
+                $userId = self::$user->user_id;
+                if($userId instanceof Error_) self::internalServerError();
+        
+                $data = self::getParams();
+                $data = self::getParamsWithoutUserToken($data);
+                $data['cart_uid'] = $userId;
+        
+                $result = \Model\Cart::count($data);
+                if($result instanceof Error_) self::badRequest();
+
+                echo json_encode($result); exit;
+            }
+
+            default: {
+                $userId = self::$user->user_id;
+                if($userId instanceof Error_) self::internalServerError();
+        
+                $data = self::getParams();
+                $data = self::getParamsWithoutUserToken($data);
+                $data['cart_uid'] = $userId;
+        
+                $result = \Model\Cart::getAll($data);
+                if($result instanceof Error_) self::badRequest();
+                
+                $paramsList = array();
+                foreach($result as $key => $value){
+                    array_push($paramsList, [
+                        'cart_product_id'   => $value->cart_product_id,
+                        'cart_is_parsing'   => $value->cart_is_parsing,
+                        'cart_count'        => $value->cart_count,
+                        'cart_id'           => $value->cart_id
+                    ]);
+                }
+        
+                $products = self::getProductsCart($paramsList);
+        
+                echo json_encode($products);exit;
+            }
+        }	
+    }
 
     protected static function post(){
         $userId = self::$user->user_id;
@@ -66,31 +85,104 @@ class cart extends \API\AController {
         $data = self::getParamsWithoutUserToken($data);
 
         $data['cart_uid'] = $userId;
+        $idProduct = $data['cart_product_id'];
 
         if(!self::checkIsValidPostRequest($data)) self::badRequest();
         if(!self::checkParam($data['cart_uid'])) self::internalServerError();
 
-        
-        $result = \Model\Cart::create($data);
+        $checkArr = array(
+            'cart_uid'          => $userId,
+            'cart_product_id'   => $idProduct,
+        );
 
-        $cartInfo = \Model\Cart::get($result);
-        $idProduct = $cartInfo->cart_product_id;
-        $isParser = $cartInfo->cart_is_parsing;
-        $countProduct = $cartInfo->cart_count;
+        $checkCount = \Model\Cart::count($checkArr);
 
-        if($isParser != 0){
-            $product = \Model\ParsingProduct::get($idProduct);
+        if($checkCount === 0){
+            $result = \Model\Cart::create($data);
+            $cartInfo = \Model\Cart::get($result);
+            $idProduct = $cartInfo->cart_product_id;
+            $isParser = $cartInfo->cart_is_parsing;
+            $countProduct = $cartInfo->cart_count;
+            $cartId = $cartInfo->cart_id;
+
+            if($isParser != 0){
+                $product = \Model\ParsingProduct::get($idProduct);
+            }
+
+            $product = self::getProductWithParams($product, array(
+                'count_cart'    => $countProduct,
+                'cart_id'       => $cartId,
+            ));
+
+            echo json_encode($product);exit;
+            
+        } else {
+            $elemCartList = \Model\Cart::getAll($checkArr);
+            $indexFirstProducts = array_key_first($elemCartList);
+            $idCartProduct = $elemCartList[$indexFirstProducts]->cart_id;
+            $product = \Model\Cart::get($idCartProduct);
+            $updateData['cart_count'] = $product->cart_count + $data['cart_count'];
+            $product->update($updateData);
+
+            $isParser = $product->cart_is_parsing;
+            $countProduct = $product->cart_count;
+            $cartId = $product->cart_id;
+
+            if($isParser != 0){
+                $product = \Model\ParsingProduct::get($idProduct);
+            }
+
+            $product = self::getProductWithParams($product, array(
+                'count_cart'    => $countProduct,
+                'cart_id'       => $cartId,
+            ));
+
+            echo json_encode($product);exit;
         }
-
-        $product['count'] = $countProduct;
-
-
-        echo json_encode($product);exit;
     }
 
     protected static function checkIsValidPostRequest($data){
         return  self::checkParam($data['cart_product_id']) && 
                 self::checkParam($data['cart_is_parsing']) && 
                 self::checkParam($data['cart_count']);
+    }
+
+    protected static function getProductsCart($paramsList){
+        $products = array();
+
+        foreach($paramsList as $key => $value){
+        
+            $idProduct = $value['cart_product_id'];
+            $isParser = $value['cart_is_parsing'];
+            $countProduct = $value['cart_count'];
+            $cartId = $value['cart_id'];
+
+            if($isParser != 0){
+                $product = \Model\ParsingProduct::get($idProduct);
+            }
+
+            $product = self::getProductWithParams($product, array(
+                'cart_id' => $cartId,
+                'count_cart' => $countProduct,
+            ));
+
+            array_push($products, $product);
+        }
+
+        return $products;
+    }
+
+    protected static function getProductWithParams($product, $params){
+        $currectProduct = array();
+
+        foreach($product as $key => $value){
+            $currectProduct[$key] = $value;
+        }
+
+        foreach($params as $key => $value){
+            $currectProduct[$key] = $value;
+        }
+
+        return $currectProduct;
     }
 }
